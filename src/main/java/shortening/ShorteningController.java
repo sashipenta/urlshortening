d561634c8dd48bcf04abd29a4d19a169f52a1624
Sashi Penta <sashi.kumar.penta@intel.com>
2017-04-19 16:05:59 -0700

    v0.1 returns fixed code

--- src/main/java/shortening/ShorteningController.java
+++ src/main/java/shortening/ShorteningController.java
@@ -6,17 +6,41 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
 
 @RestController
 public class ShorteningController
 {
-    private static final String code = "defaultCode";
+    private final Map<String, String> CodeToUrl = new HashMap<String, String>();
+    private final Map<String, String> UrlToCode = new HashMap<String, String>();
+
+    private final AtomicLong index = new AtomicLong();
+
+    private String getCode(long idx)
+    {
+        return "abcdefg";
+        // return 7 character long code
+    }
 
     // TODO: restrict to post
     // TODO: make url mandatory
     @RequestMapping("/shortening")
-    public URLObject shortening(@RequestParam(value="url", defaultValue="defaultValue") String name)
+    public URLObject shortening(@RequestParam(value="url", defaultValue="defaultValue") String url)
     {
-        return new URLObject(code, name);
+
+        if(UrlToCode.containsKey(url))
+        {
+            String code = UrlToCode.get(url);
+            return new URLObject(code, url);
+        }
+        else
+        {
+            String code = getCode(index.incrementAndGet());    
+            
+            CodeToUrl.put(url, code);
+            UrlToCode.put(code, url);
+
+            return new URLObject(code, url);
+        }            
     }
 }
